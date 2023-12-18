2d30e60b4a6af957e26cb702dcf2aeda9709fb9b
Geawher <jawherjabri2000@gmail.com>
2023-12-18 10:32:48 +0100

    model + tests

--- starter/model_card_template.md
+++ starter/model_card_template.md
@@ -3,16 +3,21 @@
 For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
 ## Model Details
-
+The used model for preidiction is a RandomForest classifier with default hyperparameters using the Sklearn library.
 ## Intended Use
-
+This model demonstrates the capability to predict if an individual's salary level is higher or lower than 50K/yr by leveraging different features.
 ## Training Data
-
+The training data is from the UCI Machine Learning Repository. The data set contains 48,842 instances and 14 attributes. It is available at [here](https://archive.ics.uci.edu/ml/datasets/census+income)
 ## Evaluation Data
-
+For the evaulation data sliced the main dataset into train and test, with a 80/20 split. The test data is used to evaluate the model's performance.
+As for the processing of the data, we put in place categorical encoding using onehot encoding and binazier was used for the target variable.
 ## Metrics
-_Please include the metrics used and your model's performance on those metrics._
-
+The model achieved the following scores:
+```
+Precision: 0.75
+Recall: 0.63
+Fbeta: 0.68
 ## Ethical Considerations
-
+Model performance should be treated and analyzed with caution as the training features encompass sensitive information like race and sex. This inclusion may introduce biases in predictions that warrant careful consideration.
 ## Caveats and Recommendations
+The intended use of the model is solely for predicting income based on census data, and it is not designed for any other purposes.
--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,5 +1,7 @@
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -18,7 +20,11 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    model = Pipeline(
+        steps=[("classifier", RandomForestClassifier())]
+    )
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +63,51 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    return model.predict(X)
+
+def compute_slices(dataframe, feature, y, preds):
+    """
+    Compute the performance on slices for a given categorical feature.
+
+    Inputs:
+    Dataframe: pandas DataFrame
+        Test dataframe pre-processed with features, including the categorical feature for slicing
+        then save the dataframe into a text file appending each time for each feature.
+    feature: str
+        Feature on which to perform the slices.
+    y : np.array
+        Corresponding known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+
+    Returns:
+    Dataframe with columns:
+        feature value: value of the categorical feature
+        n_samples: number of data samples in the slice
+        precision : precision score
+        recall : recall score
+        fbeta : fbeta score
+    """
+    slice_options = dataframe[feature].unique()
+    performance_data = []
+    save_path = './starter/starter/ml/slice_output.txt'
+
+    for option in slice_options:
+        slice_mask = dataframe[feature] == option
+        slice_y = y[slice_mask]
+        slice_preds = preds[slice_mask]
+
+        precision = precision_score(slice_y, slice_preds)
+        recall = recall_score(slice_y, slice_preds)
+        fbeta = fbeta_score(slice_y, slice_preds, beta=1)
+
+        performance_data.append({
+            'feature value': option,
+            'n_samples': len(slice_y),
+            'precision': precision,
+            'recall': recall,
+            'fbeta': fbeta
+        })
+
+    performance_df = pd.DataFrame(performance_data)
+    performance_df.to_csv(save_path, mode='a', index=False)
